a539b18a7eb838cce6ea79aef67b385d2d0c4506
PraveenPuvvula <praveenpuvvula43@gmail.com>
2020-12-28 21:13:16 -0500

    added class implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Hooks!
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonText: "this is the initial text",
+    };
+  }
+
+  buttonClick = () => {
+    this.setState({ buttonText: "this is updated text" });
+  };
+
+  render() {
+    const { buttonText } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button onClick={this.buttonClick}>{buttonText}</button>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
